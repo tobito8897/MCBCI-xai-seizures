080d3057d343022b49f336ee4cd72178695c4ebf
Sergio <tobito.8897@gmail.com>
2022-04-04 21:12:11 -0500

    script modified

--- ksh/run_leoatrox_job.sh
+++ ksh/run_leoatrox_job.sh
@@ -5,4 +5,27 @@ DATAPATH=`echo /../bin`
 cd $SCRIPTPATH$DATAPATH
 
 /lustre/home/ssanchez/python-core_375/bin/python3 train_ml_model_crossseizure_wang.py --patient=chb01 --db=chb-mit --model=wang_1d
+/lustre/home/ssanchez/python-core_375/bin/python3 train_ml_model_crossseizure_wang.py --patient=chb02 --db=chb-mit --model=wang_1d
+/lustre/home/ssanchez/python-core_375/bin/python3 train_ml_model_crossseizure_wang.py --patient=chb03 --db=chb-mit --model=wang_1d
+/lustre/home/ssanchez/python-core_375/bin/python3 train_ml_model_crossseizure_wang.py --patient=chb04 --db=chb-mit --model=wang_1d
+/lustre/home/ssanchez/python-core_375/bin/python3 train_ml_model_crossseizure_wang.py --patient=chb05 --db=chb-mit --model=wang_1d
+/lustre/home/ssanchez/python-core_375/bin/python3 train_ml_model_crossseizure_wang.py --patient=chb06 --db=chb-mit --model=wang_1d
+/lustre/home/ssanchez/python-core_375/bin/python3 train_ml_model_crossseizure_wang.py --patient=chb07 --db=chb-mit --model=wang_1d
+/lustre/home/ssanchez/python-core_375/bin/python3 train_ml_model_crossseizure_wang.py --patient=chb08 --db=chb-mit --model=wang_1d
+/lustre/home/ssanchez/python-core_375/bin/python3 train_ml_model_crossseizure_wang.py --patient=chb09 --db=chb-mit --model=wang_1d
+/lustre/home/ssanchez/python-core_375/bin/python3 train_ml_model_crossseizure_wang.py --patient=chb10 --db=chb-mit --model=wang_1d
+/lustre/home/ssanchez/python-core_375/bin/python3 train_ml_model_crossseizure_wang.py --patient=chb11 --db=chb-mit --model=wang_1d
+/lustre/home/ssanchez/python-core_375/bin/python3 train_ml_model_crossseizure_wang.py --patient=chb12 --db=chb-mit --model=wang_1d
+/lustre/home/ssanchez/python-core_375/bin/python3 train_ml_model_crossseizure_wang.py --patient=chb13 --db=chb-mit --model=wang_1d
+/lustre/home/ssanchez/python-core_375/bin/python3 train_ml_model_crossseizure_wang.py --patient=chb14 --db=chb-mit --model=wang_1d
+/lustre/home/ssanchez/python-core_375/bin/python3 train_ml_model_crossseizure_wang.py --patient=chb15 --db=chb-mit --model=wang_1d
+/lustre/home/ssanchez/python-core_375/bin/python3 train_ml_model_crossseizure_wang.py --patient=chb16 --db=chb-mit --model=wang_1d
+/lustre/home/ssanchez/python-core_375/bin/python3 train_ml_model_crossseizure_wang.py --patient=chb17 --db=chb-mit --model=wang_1d
+/lustre/home/ssanchez/python-core_375/bin/python3 train_ml_model_crossseizure_wang.py --patient=chb18 --db=chb-mit --model=wang_1d
+/lustre/home/ssanchez/python-core_375/bin/python3 train_ml_model_crossseizure_wang.py --patient=chb19 --db=chb-mit --model=wang_1d
+/lustre/home/ssanchez/python-core_375/bin/python3 train_ml_model_crossseizure_wang.py --patient=chb20 --db=chb-mit --model=wang_1d
+/lustre/home/ssanchez/python-core_375/bin/python3 train_ml_model_crossseizure_wang.py --patient=chb21 --db=chb-mit --model=wang_1d
+/lustre/home/ssanchez/python-core_375/bin/python3 train_ml_model_crossseizure_wang.py --patient=chb22 --db=chb-mit --model=wang_1d
+/lustre/home/ssanchez/python-core_375/bin/python3 train_ml_model_crossseizure_wang.py --patient=chb23 --db=chb-mit --model=wang_1d
+/lustre/home/ssanchez/python-core_375/bin/python3 train_ml_model_crossseizure_wang.py --patient=chb24 --db=chb-mit --model=wang_1d
 deactivate
